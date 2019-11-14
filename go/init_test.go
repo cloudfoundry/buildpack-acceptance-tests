336f2d206a136afe0ee4e36a0379035f4cdd68c8
Daniel Thornton <dthornton@pivotal.io>
2019-11-14 16:51:20 -0500

    add empty go tests to get integration to pass
    Co-authored-by: Ryan Moran <rmoran@pivotal.io>

---
+++ go/init_test.go
@@ -0,0 +1,27 @@
+package integration_test
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var packagedBuildpack cutlass.VersionedBuildpackPackage
+
+func init() {
+	flag.StringVar(&packagedBuildpack.File, "buildpack", "", "path to a packaged buildpack")
+	flag.StringVar(&packagedBuildpack.Version, "buildpack-version", "", "version of the packaged buildpack")
+	flag.BoolVar(&cutlass.Cached, "cutlass.cached", true, "cached buildpack")
+	flag.StringVar(&cutlass.DefaultMemory, "memory", "128M", "default memory for pushed apps")
+	flag.StringVar(&cutlass.DefaultDisk, "disk", "256M", "default disk for pushed apps")
+	flag.Parse()
+}
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Go Integration Suite")
+}
